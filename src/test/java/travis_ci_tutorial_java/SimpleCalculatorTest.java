600ddd44abb2c1a8b9d62814a2e13bf6342b5c54
Matt Dibbern <mdibbern14@gmail.com>
2025-03-06 16:06:36 -0500

    added divide by 0 test

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -12,23 +12,27 @@ public class SimpleCalculatorTest {
 		assertEquals(calc.add(1, 1), 2);
 	}
 	
+	@Test
 	public void testSubtract() {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.subtract(5, 3), 2);
 	}
 	
+	@Test
 	public void testMultiply() {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.multiply(6, 2), 12);
 	}
 	
+	@Test
 	public void testDivide() {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.divide(6, 2), 3);
 	}
 	
+	@Test
 	public void testDivideByZero() {
 		SimpleCalculator calc = new SimpleCalculator();
-		assertEquals(calc.divide(6,  0), -1);
+		assertEquals(calc.divide(5, 0), 0);
 	}
 }
